712e4e65bbdfe4adae63b9e76dd6ea32c45c0414
chrischarters <chartersauce@gmail.com>
2016-06-01 23:58:11 -0400

    This update icludes saves to the HAB Test program and the cansat_mbed library.

--- tests/HAB/hab_test.cpp
+++ tests/HAB/hab_test.cpp
@@ -10,17 +10,21 @@ int main()
         {
             xbee.printf("Command received...\r\n"); // xbee is readable, command received
             char c = xbee.getc();                   // take in received character
-            if (c = 'r')                            // check that command is release
+            if (c == 'n')                            // check that command is release - 'n'
             {
                 xbee.printf("Activating MOSFET...");
                 mosfet.write(1);                    // drive mosfet pin HIGH
+                wait(10);                            // wait certain amount of time, function takes in seconds
                 xbee.printf("Done.\r\n");
-                wait(5);                            // wait certain amount of time, function takes in seconds
                 mosfet.write(0);                    // drive mosfet pin LOW
-            } else {
+            }
+            else
+            {
                 xbee.printf("Unrecognized command.\r\n");   // if command isn't correct
             }
-        } else {
+        }
+        else
+        {
             xbee.printf("Awaiting command...\r\n"); // xbee not readable, no command received
         }
     }
